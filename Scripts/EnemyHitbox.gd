e21f833ab59701dcab243087a312d9e5f8cd7e8a
YKingu <63752533+YKingu@users.noreply.github.com>
2026-08-15 19:01:44 +0200

    tescommit

--- Scripts/EnemyHitbox.gd
+++ Scripts/EnemyHitbox.gd
@@ -6,6 +6,6 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	var collision_object = get_last_slide_collision()
 	
-	if collision_object != null:
+	if collision_object != null: 
 		if collision_object.get_collider() is PCBehaviour:
 			LevelInfo.main_scene.reload_level()
